119aa6ca52fe473fe0d3465fdc280c2782742dc1
Glitch <caleb.samuel.brown@gmail.com>
2021-10-26 17:21:21 +0200

    commit so I can access from pc

--- Caleb-Brown-TE20A-FightSim/Program.cs
+++ Caleb-Brown-TE20A-FightSim/Program.cs
@@ -8,13 +8,15 @@ int hp1 = 100, hp2 = 100;
 int damage1 = 0, damage2 = 0;
 int money = 1000;
 bool simStart = false, gameStart = true;
-double hitChance = 0f;
+double doubleRand = 0f;
+double hitChance1 = 0f, hitChance2 = 0f;
 int wager = 0;
 int damageModifier1 = 1, damageModifier2 = 1;
 ConsoleKey chooseFighter = ConsoleKey.E;
-int minDmg1 = 5, minDmg2 = 5, maxDmg1 = 50, maxDmg2 = 50;
+int minDmg1 = 5, minDmg2 = 5, maxDmg1 = 25, maxDmg2 = 25;
 
 int nameChoice1 = 0, nameChoice2 = 0, nameChoice3 = 0;
+
 
 Random generator = new Random();
 
@@ -23,11 +25,13 @@ string[] names = File.ReadAllLines(@"names.txt");
 Console.WriteLine("Hello and welcome to this epic and awesome battle simulator");
 Console.WriteLine("You will partake in 5 different battles where you can wager for or against your fighter");
 Console.WriteLine("After each round you can improve your fighter with the money you've accumulated");
+Console.WriteLine("Bare in mind that the more you play the more your opponent will also improve");
 Console.WriteLine("If you wish to participate again you can do so while retaining your stats");
 Console.ReadLine();
 Console.Clear();
 
-while (gameStart == true)
+nextRound:
+while (gameStart == true && round <= 5)
 {
     Start();
 }
@@ -35,6 +39,7 @@ while (gameStart == true)
 while (simStart == true)
 {
     Fight();
+    goto nextRound;
 }
 
 void Start()
@@ -78,6 +83,10 @@ void Start()
         }
     }
 
+
+
+    chooseFighter = ConsoleKey.E;
+
     gameStart = false;
     simStart = true;
 }
@@ -93,13 +102,20 @@ void Fight()
         damage1 = generator.Next(minDmg1, maxDmg1) * damageModifier1;
         damage2 = generator.Next(minDmg2, maxDmg2) * damageModifier2;
 
-        // hitChance = generator.NextDouble();
 
-        Console.WriteLine(damage1);
-        Console.WriteLine(damage2);
 
-        hp1 -= damage2;
-        hp2 -= damage1;
+        doubleRand = generator.NextDouble();
+        if (doubleRand < 0.75)
+        {
+            hp1 -= damage2;
+        }
+
+        doubleRand = generator.NextDouble();
+        if (doubleRand < 0.75)
+        {
+            hp2 -= damage1;
+        }
+
 
         if (hp1 < 0)
         {
@@ -114,7 +130,7 @@ void Fight()
         Console.WriteLine($"{fighter1} has {hp1}hp remaining");
         Console.WriteLine($"{fighter2} has {hp2}hp remaining");
 
-        simStart = false;
+
         Console.ReadLine();
     }
 
@@ -126,19 +142,28 @@ void Fight()
         Console.WriteLine("Both fighters were knocked unconcious");
         Console.ReadLine();
     }
-
-    if (hp1 <= 0)
+    else if (hp1 <= 0)
     {
         Console.WriteLine($"{fighter1} down");
         Console.WriteLine($"{fighter2} wins!");
         Console.ReadLine();
 
     }
-
-    if (hp2 <= 0)
+    else if (hp2 <= 0)
     {
         Console.WriteLine($"{fighter2} down");
         Console.WriteLine($"{fighter1} wins");
         Console.ReadLine();
     }
+
+    Console.WriteLine("Next round will start once you pick your next opponent");
+    Console.ReadLine();
+    round++;
+    gameStart = true;
+    simStart = false;
+
+
+
+    hp1 = 100;
+    hp2 = 100;
 }
